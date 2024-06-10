f053e729f492dab731e85cb5580d5e74b04580de
ZeyadMahfouzz <168578483+ZeyadMahfouzz@users.noreply.github.com>
2024-06-10 21:53:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 
-data = pd.read_csv("/workspaces/SpamDetection/sms_spam.csv")
+data = pd.read_csv("D:\Zeyad\Downloads\sms_spam.csv")
 x = data['text']
 y = data['type']
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.25,random_state=0)
@@ -17,7 +17,7 @@ clfr.fit(sms_train_vectorized,y_train)
 predicted = clfr.predict(sms_test_vectorized)
 
 st.title("Spam Classification App")
-st.image("/workspaces/SpamDetection/SpamIMG.png",use_column_width=True)
+st.image("D:\Zeyad\Downloads\SpamIMG.png",use_column_width=True)
 st.text('Model Description: Naive Bayes Model, trained on sms data')
 text = st.text_input("Enter Text Here","Type Here...")
 predict = st.button('Predict')
